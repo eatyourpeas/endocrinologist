a67a69be166675b6dde44571f3488f357544e31f
eatyourpeas <simon@eatyourpeas.co.uk>
2025-06-27 10:57:50 +0100

    gradle updates

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,6 +24,7 @@ android {
     namespace = "uk.co.eatyourpeas.endocrinologist"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
